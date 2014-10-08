dde621a837a4118838fde6901f209a85424d2b76
eh222ve <eh222ve@student.lnu.se>
2014-10-08 15:40:03 +0200

    Added MyExtensions Class

---
+++ _1DV402.S2.L02C/MyExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _1DV402.S2.L02C.Properties;
+
+namespace _1DV402.S2.L02C
+{
+    public class MyExtensions
+    {
+        //Displays message to user
+        public static void ViewMessage(string message, ConsoleColor backgroundColor = ConsoleColor.Blue, ConsoleColor foregroundColor = ConsoleColor.White)
+        {
+            MyExtensions.ChangeColor(backgroundColor, foregroundColor);
+
+            Console.WriteLine(message);
+
+            MyExtensions.ChangeColor(ConsoleColor.Black, ConsoleColor.White);
+        }
+
+        //Changes appearance of console window
+        public static void PrettyConsole(string title, ConsoleColor backgroundColor = ConsoleColor.White, ConsoleColor foregroundColor = ConsoleColor.Black)
+        {
+            Console.Title = title;
+            ChangeColor(foregroundColor, backgroundColor);
+            Console.Clear();
+        }
+
+
+        //Changes appearance of console text
+        public static void ChangeColor(ConsoleColor foregroundColor = ConsoleColor.Black, ConsoleColor backgroundColor = ConsoleColor.White)
+        {
+            Console.BackgroundColor = backgroundColor;
+            Console.ForegroundColor = foregroundColor;
+        }
+    }
+}
--- _1DV402.S2.L02C/Program.cs
+++ _1DV402.S2.L02C/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Media;
 using _1DV402.S2.L02C.Properties;
 
 namespace _1DV402.S2.L02C
@@ -11,6 +12,41 @@ namespace _1DV402.S2.L02C
     {
         static void Main(string[] args)
         {
+            MyExtensions.PrettyConsole("Alarmklockan");
+
+            MyExtensions.ChangeColor(ConsoleColor.White, ConsoleColor.DarkMagenta);
+            Console.WriteLine(" ╔═════════════════════════════════╗ ");
+            Console.WriteLine(" ║     Alarmklockan URLED (TM)     ║ ");
+            Console.WriteLine(" ║    Modellnummer: 1DV402S2L2C    ║ ");
+            Console.WriteLine(" ║      Tillverkad av: eh222ve     ║ ");
+            Console.WriteLine(" ╚═════════════════════════════════╝ ");
+            MyExtensions.ChangeColor();
+
+            AlarmClock ac = new AlarmClock("20:56", new string[3] { "20:57", "21:00", "21:12" });
+
+            Console.WriteLine("Test 1:");
+            Console.WriteLine("Test av standardkonstruktorn");
+            try
+            {
+                AlarmClock t1AC = new AlarmClock();
+                Console.WriteLine(t1AC.ToString());
+            }
+            catch (Exception)
+            {
+                MyExtensions.ViewMessage("fel", ConsoleColor.Red);
+            }
+
+            for (int i = 0; i < 20; i++)
+            {
+                if (ac.TickTock())
+                {
+                    
+                    Console.WriteLine("Alarms went off");
+                }
+                Console.WriteLine(ac.ToString());
+            }
+
+            Console.ReadLine();
         }
     }
 }
